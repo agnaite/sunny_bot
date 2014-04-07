af684ceb7ef3d9d0a6971e215a590c01e63a674d
audy <harekrishna@gmail.com>
2014-04-06 21:46:57 -0400

    it gets the temperature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '..'))
 
 require 'environment'
 
-Bundler.require :test
+Bundler.require :test, :development
 
 require 'latest_temperature'
